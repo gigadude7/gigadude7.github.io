e5939b7e32323779bc693bca877ac25c21078b46
Grant Cordle <you@example.com>
2015-10-11 20:33:43 -0500

    init commit of distributed weaglebook and some DND stuff

---
+++ Software Construction/COMP 2710/lab 2/Distributed WeagleBook/Distributed WeagleBook/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Distributed WeagleBook
+//
+//  Created by Grant Hamilton Cordle on 10/11/15.
+//  Copyright © 2015 Grant Hamilton Cordle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
